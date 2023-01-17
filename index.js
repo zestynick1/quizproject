c91e690583036fe377360106a6bd5d284952d59a
zestynick1 <112553783+zestynick1@users.noreply.github.com>
2023-01-17 12:25:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,118 @@
-body{
-	font-size: 20px;
-	font-family: sans-serif;
-	color: #333;
-
-}
-.question{
-	font-weight: 600
-}
-.answers {
-    margin-bottom: 20px;
-}
-#submit{
-	font-family: sans-serif;
-	font-size: 20px;
-	background-color: #297;
-	color: rgb(254, 254, 254);
-	border: 0px;
-	border-radius: 3px;
-	padding: 20px;
-	cursor: pointer;
-	margin-bottom: 20px;
-}
-#submit:hover{
-	background-color: rgb(90, 51, 81);
-}
-body{
-   background-color: rgb(223, 85, 145);
-   }
-
+var myQuestions = [
+    {
+      question: "What is the capital of Somalia?",
+      answers: {
+        a: 'Asmara',
+        b: 'Mogadishu',
+        c: 'Nairobi'
+      },
+      correctAnswer: 'b'
+    },
+    {
+      question: "What is the capital of Democratic republic of the Congo?",
+      answers: {
+        a: 'Kinchasa',
+        b: 'Windheok',
+        c: 'Cairo'
+      },
+      correctAnswer: 'a'
+    },
+    {
+      question: "What is the capital of the Phillipines?",
+      answers: {
+        a: 'Jakarta',
+        b: 'Kuala Lumpur',
+        c: 'Manila'
+      },
+      correctAnswer: 'c'
+    },
+  ];
+  
+  var quizContainer = document.getElementById('quiz');
+  var resultsContainer = document.getElementById('results');
+  var submitButton = document.getElementById('submit');
+  
+  generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+  
+  function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+  
+    function showQuestions(questions, quizContainer){
+      // we'll need a place to store the output and the answer choices
+      var output = [];
+      var answers;
+  
+      // for each question...
+      for(var i=0; i<questions.length; i++){
+        
+        // first reset the list of answers
+        answers = [];
+  
+        // for each available answer...
+        for(letter in questions[i].answers){
+  
+          // ...add an html radio button
+          answers.push(
+            '<label>'
+              + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+              + letter + ': '
+              + questions[i].answers[letter]
+            + '</label>'
+          );
+        }
+  
+        // add this question and its answers to the output
+        output.push(
+          '<div class="question">' + questions[i].question + '</div>'
+          + '<div class="answers">' + answers.join('') + '</div>'
+        );
+      }
+  
+      // finally combine our output list into one string of html and put it on the page
+      quizContainer.innerHTML = output.join('');
+    }
+  
+  
+    function showResults(questions, quizContainer, resultsContainer){
+      
+      // gather answer containers from our quiz
+      var answerContainers = quizContainer.querySelectorAll('.answers');
+      
+      // keep track of user's answers
+      var userAnswer = '';
+      var numCorrect = 0;
+      
+      // for each question...
+      for(var i=0; i<questions.length; i++){
+  
+        // find selected answer
+        userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+        
+        // if answer is correct
+        if(userAnswer===questions[i].correctAnswer){
+          // add to the number of correct answers
+          numCorrect++;
+          
+          // color the answers green
+          answerContainers[i].style.color = 'lightgreen';
+        }
+        // if answer is wrong or blank
+        else{
+          // color the answers red
+          answerContainers[i].style.color = 'red';
+        }
+      }
+  
+      // show number of correct answers out of total
+      resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+    }
+  
+    // show questions right away
+    showQuestions(questions, quizContainer);
+    
+    // on submit, show results
+    submitButton.onclick = function(){
+      showResults(questions, quizContainer, resultsContainer);
+    }
+  
+  }
+    
